02f2973c2d358f411b460a6d27d23a55ca819a3a
Abd-Abo-Alqomboz <80759403+Abd-Abo-Alqomboz@users.noreply.github.com>
2022-11-12 13:40:04 +0200

    abd

--- provider/abd.js/shimal.js
+++ provider/abd.js/shimal.js
@@ -1,3 +1,4 @@
+/** start butone for page oreder */
 function info_1() {
   window.location.assign("order_1.html")
 }
@@ -10,3 +11,29 @@ function info_3() {
 function info_4() {
   window.location.assign("order_4.html")
 }
+function close_1() {
+  window.location.assign("order_1.html")
+}
+function close_2() {
+  window.location.assign("order_2.html")
+}
+function close_3() {
+  window.location.assign("order_3.html")
+}
+function close_4() {
+  window.location.assign("order_4.html")
+}
+/** start butone for page oreder */
+/** start butone for page addresses */
+function close_5() {
+  window.location.assign("addresses.html")
+}
+/** end butone for page addresses */
+/** start butone for page boeket */
+function close_6() {
+  window.location.assign("boeket.html")
+}
+/** end butone for page boeket */
+
+
+
