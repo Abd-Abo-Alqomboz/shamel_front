a29121744ceef196a52796566ad9aa1d13008791
Abd-Abo-Alqomboz <80759403+Abd-Abo-Alqomboz@users.noreply.github.com>
2022-11-12 10:34:09 +0200

    abd

---
+++ provider/abd.js/shimal.js
@@ -0,0 +1,12 @@
+function info_1() {
+  window.location.assign("order_1.html")
+}
+function info_2() {
+  window.location.assign("order_2.html")
+}
+function info_3() {
+  window.location.assign("order_3.html")
+}
+function info_4() {
+  window.location.assign("order_4.html")
+}
